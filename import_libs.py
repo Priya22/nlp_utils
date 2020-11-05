d9980cd39eac5460054148b4aa0ea24a8f55f686
Krishnapriya Vishnubhotla <vk22priya@gmail.com>
2020-11-05 10:26:20 -0500

    utility libraries to always import

---
+++ import_libs.py
@@ -0,0 +1,24 @@
+import sklearn 
+import importlib
+import os, re, sys, csv, json
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import string
+
+import random
+
+import math
+
+from tqdm import tqdm
+
+from collections import Counter
+
+import pickle as pkl
+
+%matplotlib inline
+
+from sklearn import metrics
+import clustering_utils
+import emb_utils
